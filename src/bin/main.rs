2cbde0c3a8fd340ca5c5bd4337a8f2d699558127
Ryan Estep <rmestep@bex.net>
2025-02-01 23:03:18 +1300

    Initial implementation of interrupt handler & PWM reading bitbang
    - Still need to push events to a queue and implement main-loop part of
      the calc to avoid doing more than neccicary in the ISR

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,14 +1,28 @@
 #![no_std]
 #![no_main]
 
+use core::cell::RefCell;
+use critical_section::Mutex;
 use esp_backtrace as _;
 use esp_hal::clock::CpuClock;
 use esp_hal::delay::Delay;
-use esp_hal::main;
+use esp_hal::interrupt::InterruptConfigurable;
+use esp_hal::timer::systimer::SystemTimer;
 use esp_hal::timer::timg::TimerGroup;
+use esp_hal::timer::{AnyTimer, Timer};
+
+use esp_hal::{
+    gpio::{Event, Input, Io, Pull},
+    handler, main,
+};
 use log::info;
 
 extern crate alloc;
+
+static GLOBAL_TIMER: Mutex<RefCell<Option<AnyTimer>>> = Mutex::new(RefCell::new(None));
+
+static LEFT_DRIVE_INPUT: Mutex<RefCell<Option<Input>>> = Mutex::new(RefCell::new(None));
+static RIGHT_DRIVE_INPUT: Mutex<RefCell<Option<Input>>> = Mutex::new(RefCell::new(None));
 
 #[main]
 fn main() -> ! {
@@ -19,8 +33,8 @@ fn main() -> ! {
 
     esp_println::logger::init_logger_from_env();
 
+    // Initalize the WiFi subsystem
     esp_alloc::heap_allocator!(72 * 1024);
-
     let timg0 = TimerGroup::new(peripherals.TIMG0);
     let _init = esp_wifi::init(
         timg0.timer0,
@@ -29,6 +43,28 @@ fn main() -> ! {
     )
     .unwrap();
 
+    let mut gpio_io = Io::new(peripherals.IO_MUX);
+
+    // Initalize the PWM Input subsystem
+    let sys_clock = SystemTimer::new(peripherals.SYSTIMER);
+    let mut gpio9 = Input::new(peripherals.GPIO9, Pull::None);
+    let mut gpio8 = Input::new(peripherals.GPIO8, Pull::None);
+    critical_section::with(|cs| {
+        sys_clock.alarm0.start();
+        GLOBAL_TIMER
+            .borrow_ref_mut(cs)
+            .replace(AnyTimer::from(sys_clock.alarm0));
+
+        gpio9.listen(Event::AnyEdge);
+        LEFT_DRIVE_INPUT.borrow_ref_mut(cs).replace(gpio9);
+
+        gpio8.listen(Event::AnyEdge);
+        RIGHT_DRIVE_INPUT.borrow_ref_mut(cs).replace(gpio8);
+    });
+
+    // bind the interrupt handler so GPIO interrupts are captured
+    gpio_io.set_interrupt_handler(gpio_handler);
+
     let delay = Delay::new();
     loop {
         info!("Hello world!");
@@ -36,4 +72,15 @@ fn main() -> ! {
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/v0.23.1/examples/src/bin
+}
+
+#[handler]
+fn gpio_handler() {
+    critical_section::with(|cs| {
+        let timestamp = GLOBAL_TIMER.borrow_ref_mut(cs).as_mut().unwrap().now();
+
+        // TODO: store left & right PWM event into a queue with the timestamp for the main loop to
+        // pop and convert to calculate the pulse width / duty cycle.
+        // Both pins need to be read because the handler is triggered for either pin.
+    });
 }
